e42779fb258db730533138f8e246f49b06b8263c
zhoxie <xiezhongjie1109@gmail.com>
2021-08-06 13:56:37 +0800

    add mats frontend and gms docker build file

---
+++ docker/mats-gms/start.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# Add default URI (http) scheme if needed
+if ! echo $NEO4J_HOST | grep -q "://" ; then
+    NEO4J_HOST="http://$NEO4J_HOST"
+fi
+
+if [[ -z $ELASTICSEARCH_USERNAME ]]; then
+    ELASTICSEARCH_HOST_URL=$ELASTICSEARCH_HOST
+else
+  if [[ -z $ELASTICSEARCH_AUTH_HEADER ]]; then
+    ELASTICSEARCH_HOST_URL=$ELASTICSEARCH_USERNAME:$ELASTICSEARCH_PASSWORD@$ELASTICSEARCH_HOST
+  else
+    ELASTICSEARCH_HOST_URL=$ELASTICSEARCH_HOST
+  fi
+fi
+
+# Add default header if needed
+if [[ -z $ELASTICSEARCH_AUTH_HEADER ]]; then
+    ELASTICSEARCH_AUTH_HEADER="Accept: */*"
+fi
+
+if [[ $ELASTICSEARCH_USE_SSL == true ]]; then
+    ELASTICSEARCH_PROTOCOL=https
+else
+    ELASTICSEARCH_PROTOCOL=http
+fi
+
+WAIT_FOR_NEO4J=""
+
+if [[ $GRAPH_SERVICE_IMPL != elasticsearch ]]; then
+  WAIT_FOR_NEO4J=" -wait $NEO4J_HOST "
+fi
+
+dockerize \
+  -wait tcp://$EBEAN_DATASOURCE_HOST \
+  -wait tcp://$(echo $KAFKA_BOOTSTRAP_SERVER | sed 's/,/ -wait tcp:\/\//g') \
+  -wait $ELASTICSEARCH_PROTOCOL://$ELASTICSEARCH_HOST_URL:$ELASTICSEARCH_PORT -wait-http-header "$ELASTICSEARCH_AUTH_HEADER" \
+  $WAIT_FOR_NEO4J \
+  -timeout 240s \
+  java $JAVA_OPTS $JMX_OPTS \
+  -jar /jetty-runner.jar \
+  --jar jetty-util.jar \
+  --jar jetty-jmx.jar \
+  /datahub/datahub-gms/bin/war.war
