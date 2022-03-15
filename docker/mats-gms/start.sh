ba3785cbad33d71061734438f8a2b4984d315e07
zhoxie <xiezhongjie1109@gmail.com>
2022-03-15 11:19:20 +0800

    enhance gms jetty configuration

--- docker/mats-gms/start.sh
+++ docker/mats-gms/start.sh
@@ -52,12 +52,13 @@ COMMON="
     $WAIT_FOR_NEO4J \
     -timeout 240s \
     java $JAVA_OPTS $JMX_OPTS \
+    -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005 \
     $OTEL_AGENT \
     $PROMETHEUS_AGENT \
     -jar /jetty-runner.jar \
     --jar jetty-util.jar \
     --jar jetty-jmx.jar \
-    --config /datahub/datahub-gms/scripts/jetty.xml \
+    --config jetty.xml \
     /datahub/datahub-gms/bin/war.war"
 
 if [[ $SKIP_ELASTICSEARCH_CHECK != true ]]; then
